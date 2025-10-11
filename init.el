ffac0bed840878e59f69117779d80cc8fa8eb57f
kimsaram32 <kimsaram32@proton.me>
2025-10-11 23:46:11 +0900

    20251011

--- init.el
+++ init.el
@@ -1,15 +1,23 @@
 ;;; -*- lexical-binding: t -*-
 
+(defun me:expand-emacs-file-name (name)
+  "Resolve NAME relative to `user-emacs-directory`."
+  (expand-file-name name user-emacs-directory))
+
 ;; Melpa
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Custom
+(setq custom-file (me:expand-emacs-file-name "custom.el"))
+(load custom-file)
 
 ;; Font
 ; Prevent resizing windows when font changes.
 (setq-default window-size-fixed t)
-
 (set-face-attribute 'default nil
 		    :height 150
 		    :family "JetBrains Mono")
+(setq-default window-size-fixed nil)
 
 ;; Menu bar
 (menu-bar-mode -1)
@@ -30,7 +38,23 @@
 ;; Enable visual line mode.
 (visual-line-mode)
 
+;; Make C-SPC after C-u C-SPC traverse the mark ring.
 (setq set-mark-command-repeat-pop t)
+
+;; Scratch buffer
+(setq initial-scratch-message "\
+;; Hi this is a scratch buffer
+
+")
+
+;;;;
+
+(defun me:insert-quotes (&optional arg)
+  "Like `insert-parentheses`, but for quotes."
+  (interactive "P")
+  (insert-pair arg ?\" ?\"))
+
+(keymap-global-set "M-\"" 'me:insert-quotes)
 
 ;; Org mode
 (use-package org
@@ -46,3 +70,29 @@
 (add-hook 'c-mode-hook
 	  (lambda ()
 	    (setq c-basic-offset 2)))
+
+;; Elfeed
+(defun me:elfeed-show-untag-unread ()
+  (interactive)
+  (elfeed-show-untag 'unread))
+
+(use-package elfeed :ensure t
+  :bind (("C-x w" . 'elfeed)
+	 :map elfeed-search-mode-map
+	 ("TAB" . next-line)
+	 ("S-<tab>" . previous-line)
+	 :map elfeed-show-mode-map
+	 ("r" . me:elfeed-show-untag-unread))
+  :config
+  (load (me:expand-emacs-file-name "elfeed.el"))
+  (elfeed-update))
+
+;; Modus-themes
+
+(use-package modus-themes :ensure t
+  :init
+  (require-theme 'modus-themes)
+  :bind (("<f5>" . modus-themes-rotate))
+  :config
+  (setq modus-themes-common-palette-overrides modus-themes-preset-overrides-faint)
+  (modus-themes-load-theme 'modus-vivendi-tinted))
